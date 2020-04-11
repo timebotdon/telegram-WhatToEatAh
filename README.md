ca6aa8e9284622522748fd3b8affe5f18c1426ae
Donovan Choy <choyyld@gmail.com>
2020-04-11 23:04:17 +0800

    r2 initial update

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
-# telegram-WhatToEatAh
+# telegram-WhatToEatAh (Revision 2)
 ## Required modules:
 * node-telegram-bot-api
-* mongodb
 
-## Setup (MongoDB)
+## Setup (Local JSON)
+1. Clone repo and `cd` into the folder:
+  ```bash
+  git clone https://www.github.com/timebotdon/telegram-whattoeatah
+  cd telegram-whattoeatah
+  ```
+
+2. Install required modules:
+  `npm install`
+
+3. Create a new bot and retrieve the API Token. 
+* More information here: https://core.telegram.org/bots
+
+4. Define Telegram Bot API token in `config.json`. The bot API token must be acquired from the Botfather bot.
+
+5. Run script.
+  `node app_nodb.js`
+
+## Setup (MongoDB)(DEFUNCT)
+Note that the MongoDB version will NOT be included in future revisions(r2+) as it is overkill for the scale of this project.. for now.
 ### Requirements
 * Existing MongoDB installation.
 
@@ -30,37 +48,58 @@
 6. Run script.
   `node app.js`
 
-## Setup (Local JSON)
-1. Clone repo and `cd` into the folder:
-  ```bash
-  git clone https://www.github.com/timebotdon/telegram-whattoeatah
-  cd telegram-whattoeatah
-  ```
-
-2. Install required modules:
-  `npm install`
-
-3. Create a new bot and retrieve the API Token. 
-* More information here: https://core.telegram.org/bots
-
-4. Define Telegram Bot API token in `config.json`. The bot API token must be acquired from the Botfather bot.
-
-5. Run script.
-  `node app_nodb.js`
-
 ## Bot interaction
-* Text the bot `what to eat ah` or `eat what ah` via telegram. The response will include a randomized food suggestion derived from the mongoDB database.
+* Text the bot `what to eat ah` or `eat what ah` via telegram. The response will include a randomized food suggestion derived from the JSON list/mongoDB database.
+* Text the bot `I want to eat` or `I want` to specify a filtered response. eg: `I want to eat cheaper korean fastfood` I will result in a response falling within those 3 categories (price, cuisine, foodtype) more explained on next section.
 
-## Todo
+### Responses
+#### Food type
+Food responses are now food type specific. Refer to `./lists/list_response_type.json`.
+#### Price
+This will be implemented to include price specific as well. Refer to `./lists/list_response_price.json`.
+The end goal is to send a randomized response between both categories. 
+
+#### "Thanks" repsonses
+The bot will now repond to "Thank you" messages with a randomized message. Refer to `./lists/list_response_NoProb.json`.
+
+### Input Filters
+Accepted inputs. Refer to `./lists/list_food.json` for specifications. All inputs are case insensitive.
+
+#### Foodtype
+1. fast_food - Message text: "fast food"
+2. casual_dining  - Message text: "restaurant"
+3. hawker - Message text: "hawker" or "kopitiam"
+
+#### Cuisine
+1. Chinese
+2. Malay
+3. Indian
+4. Western
+5. Korean
+6. Japanese
+7. Taiwanese
+8. Thai
+9. Vietnamese
+10. Multi (peranakan, mixed cuisines)
+
+#### Price
+1. $ - message text: "cheapest"
+2. $$ - message text: "cheaper" or "cheap"
+3. $$$ - message text: "midrange" or "mid"
+4. $$$$ - message text: "highend"
+5. $$$$$ - message text: "expensive" or "ex"
+
+# Bugs
+Yep theres still many bugs to fix at this point...
+
+## Future Features 
 Features in plan
-### Categories
+
+### rev3
+#### Categories
 * Vegetarian/Vegan
-* Cost category
+* Halal / non halal
 
-### Location
-* Location linking 
-* Restaurant/Venue recommendation
-
-### Bot Responses
-* Cuisine/Food type specific responses
-
+#### Location
+* Location linking via Google Places API
+* Restaurant/Venue recommendation via Google Places API
--- app.js
+++ app.js
@@ -1,44 +1,214 @@
 //Define modules
-const { MongoClient } = require('mongodb');
 const TelegramBot = require('node-telegram-bot-api');
+const fl = require('./lists/list_food').foodlist;
+const resType = require('./lists/list_response_type');
+const resNoProb = require('./lists/list_response_NoProb').responses;
+//const resp = require('./lists/list_response_price');
 
 //Import bot api token
 const config = require('./config.json');
 
-//define DB name
-const dbName = "fooddb";
-//define Collection name
-const collectionName = "foodlist";
-//define MongoDB url
-const url = "mongodb://localhost:27017/" + dbName;
-
 //Create a new polling bot instance
-var bot = new TelegramBot(config.teleBotToken, {polling: true});
+let bot = new TelegramBot(config.teleBotToken, {polling: true});
 
-//Bot will send message containing food data when receiving text.
-bot.onText(/(eat what|what to eat)/i, (msg) => {
-  getFood(function(finalName, finalType, finalCuisine){ //finalType & finalCuisine not implemented yet.
-    bot.sendMessage(msg.chat.id, finalName + ".");
-  })
-});
-
-// polling_error watchdog
+// Polling error watchdog
 bot.on('polling_error', (error) => {
   console.log(error);
 });
 
-// Get random food data from DB "test" and return value from function call.
-function getFood(callback){
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db(dbName);
-    dbo.collection(collectionName).aggregate([{ $sample: { size: 1 } }]).toArray(function(err,result){
-      if (err) throw err;
-      const finalName = result[0].name
-      const finalType = result[0].type
-      const finalCuisine = result[0].cuisine
-      db.close();
-      return callback(finalName, finalType, finalCuisine);
+
+//Thanks
+bot.onText(/thank/i, (msg) => {
+  const arrayLength = resNoProb.length;
+  const arrayVal = Math.floor(Math.random() * (arrayLength + 1));
+  let responseText = resNoProb[arrayVal].response;
+  bot.sendMessage(msg.chat.id, responseText);
+});
+
+
+//Bot will send message containing food data when receiving text.
+//Random
+bot.onText(/(I hungry|eat what|what to eat)/i, (msg) => {
+  getRandomFood(function(finalName, finalType, finalCuisine){
+    let arrayLength = eval("resType." + finalType + ".length");
+    const arrayVal = Math.floor(Math.random() * (arrayLength + 1))
+    let responseText = eval("resType." + finalType)
+    bot.sendMessage(msg.chat.id, responseText[arrayVal].response + " " + finalName);
+  });
+});
+
+
+//Filtered food request
+bot.onText(/(I want| I want to eat)/i, (msg) => {
+  console.log(msg.text);
+  let inputArray = msg.text.toLowerCase().split(/\s/); // Splits to [ 'i', 'want', 'to', 'eat', 'something', 'cheap' ]
+  //create new arrays
+  let identifierArray = []; 
+  let functionArray = [];
+
+  for(item=0; item<inputArray.length; item++){
+    //type filter
+    if (inputArray[item] == "fastfood"){
+      functionArray.push("filterByType");
+      identifierArray.push("fast_food");
+    }
+    if (inputArray[item] == "restaurant"){
+      functionArray.push("filterByType");
+      identifierArray.push("casual_dining");
+    }
+    if ((inputArray[item] == "kopitiam") || (inputArray[item] == "hawker")){
+      functionArray.push("filterByType");
+      identifierArray.push("hawker");
+    }
+    //cuisine filter
+    if (inputArray[item] == "western"){
+      functionArray.push("filterByCuisine");
+      identifierArray.push("western");
+    }
+    if (inputArray[item] == "chinese"){
+      functionArray.push("filterByCuisine");
+      identifierArray.push("chinese");
+    }
+    if (inputArray[item] == "indian"){
+      functionArray.push("filterByCuisine");
+      identifierArray.push("indian");
+    }
+    if (inputArray[item] == "thai"){
+      functionArray.push("filterByCuisine");
+      identifierArray.push("thai");
+    }
+    if (inputArray[item] == "korean"){
+      functionArray.push("filterByCuisine");
+      identifierArray.push("korean");
+    }
+    if (inputArray[item] == "japanese"){
+      functionArray.push("filterByCuisine");
+      identifierArray.push("japanese");
+    }
+    if ((inputArray[item] == "vietnamese") || (inputArray[item] == "viet")){
+      functionArray.push("filterByCuisine");
+      identifierArray.push("vietnamese");
+    }
+    if (inputArray[item] == "malay"){
+      functionArray.push("filterByCuisine");
+      identifierArray.push("malay");
+    }
+    //price filter
+    if (inputArray[item] == "cheapest"){
+      functionArray.push("filterByPrice");
+      identifierArray.push("$");
+    }
+    if ((inputArray[item] == "cheaper") || (inputArray[item] == "cheap")){
+      functionArray.push("filterByPrice");
+      identifierArray.push("$$");
+    }
+    if ((inputArray[item] == "midrange") || (inputArray[item] == "mid")){
+      functionArray.push("filterByPrice");
+      identifierArray.push("$$$");
+    }
+    if (inputArray[item] == "highend"){
+      functionArray.push("filterByPrice");
+      identifierArray.push("$$$$");
+    }
+    if ((inputArray[item] == "expensive") || (inputArray[item] == "ex")){
+      functionArray.push("filterByPrice");
+      identifierArray.push("$$$$$");
+    }
+  }
+
+  processInput(identifierArray, functionArray, function(finalName, finalType, finalCuisine){
+    if (typeof finalType !== 'undefined'){
+      let arrayLength = eval("resType." + finalType + ".length");
+      const arrayVal = Math.floor(Math.random() * (arrayLength + 1))
+      let responseText = eval("resType." + finalType)
+      bot.sendMessage(msg.chat.id, responseText[arrayVal].response + " " + finalName);
+    } else {
+      bot.sendMessage(msg.chat.id, "Eh I can't find what you want leh. Ask again.");
+    }
+  });
+});
+
+
+// for each element in identifierArray
+function processInput(identifierArray, functionArray, callback){
+  if (typeof identifierArray[0] !== 'undefined' ) {
+    const f0 = eval(functionArray[0] + "(identifierArray[0].toString())");
+    const random = Math.floor(Math.random() * (f0.length)) // get random from result
+    var finalName = f0[random].name;
+    var finalType = f0[random].type;
+    var finalCuisine = f0[random].cuisine;
+    if (typeof identifierArray[1] !== 'undefined' ) {
+      console.log(identifierArray);
+      const f1 = eval(functionArray[1] + "(identifierArray[1].toString(), f0)");
+      const random = Math.floor(Math.random() * (f1.length)) // get random from result
+      var finalName = f1[random].name;
+      var finalType = f1[random].type;
+      var finalCuisine = f1[random].cuisine;
+      if (typeof identifierArray[2] !== 'undefined' ) {
+        const f2 = eval(functionArray[2] + "(identifierArray[2].toString(), f1)");
+        const random = Math.floor(Math.random() * (f2.length)) // get random from result
+        var finalName = f2[random].name;
+        var finalType = f2[random].type;
+        var finalCuisine = f2[random].cuisine;
+      }
+    }
+  }
+  return callback(finalName, finalType, finalCuisine);
+}
+
+
+//filter functions output filtered arrays
+function filterByType(element, newArray){
+  if (newArray == null){
+    let filtered = fl.filter(function (el) {
+      return el.type == element;
     });
-  }); 
+    return filtered;
+  } else {
+    let filtered = newArray.filter(function (el) {
+      return el.type == element;
+    });
+    return filtered;
+  }
+}
+
+
+function filterByCuisine(element, newArray){
+  if (newArray == null){
+    let filtered = fl.filter(function (el) {
+      return el.cuisine == element;
+    });
+    return filtered;
+  } else {
+    let filtered = newArray.filter(function (el) {
+      return el.cuisine == element;
+    });
+    return filtered;
+  }
+}
+
+
+function filterByPrice(element, newArray){
+  if (newArray == null){
+    let filtered = fl.filter(function (el) {
+      return el.price == element;
+    });
+    return filtered;
+  } else {
+    let filtered = newArray.filter(function (el) {
+      return el.price == element;
+    });
+    return filtered;
+  }
+}
+
+
+// Get random food data from DB "test" and return value from function call.
+function getRandomFood(callback){
+  const randomFood = Math.floor(Math.random() * (fl.length))
+  const foodKey = fl[randomFood];
+  const finalName = foodKey.name;
+  const finalType = foodKey.type;
+  const finalCuisine = foodKey.cuisine;
+  return callback(finalName, finalType, finalCuisine);
 }
